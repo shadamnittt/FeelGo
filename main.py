b2c6f11581718fbb892c89f8490de899b8ed19d9
deli <bahiiita.5@gmail.com>
2025-05-31 00:24:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-app = ApplicationBuilder().token(BOT_TOKEN).build()
-
-# Пример команды /start
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Привет, я твой бот!")
-
-app.add_handler(CommandHandler("start", start))
-
-app.run_polling()
